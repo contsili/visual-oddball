37b4363a3b834174de529388d0dc81cf8235f99d
Konstantinos Tsilimparis <konstantinos@artinis.com>
2025-10-09 12:26:33 +0200

    fix - remove auditory oddball files

--- visual_oddball_lastrun.py
+++ visual_oddball_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2025.1.1),
-    on October 08, 2025, at 17:07
+    on October 09, 2025, at 10:55
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
